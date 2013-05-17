a53ef836448c4f60f4747458fe8e36e82bfa1d19
fanxiangyu <359419163@qq.com>
2013-05-17 22:35:16 +0800

    一些rational rose 重新导出类后生成的文件，以及一些测试用的文件
    一些rational rose 重新导出类后生成的文件，以及一些测试用的文件

---
+++ trunk/program/Can_Communication/src/com/tju/CanCommunication/test/DiscoverComm.java
@@ -0,0 +1,62 @@
+package com.tju.CanCommunication.test;
+
+import java.util.Enumeration;
+import java.util.HashSet;
+
+import gnu.io.*;
+
+public class DiscoverComm {
+
+	static void listPorts() {
+		java.util.Enumeration<CommPortIdentifier> portEnum = CommPortIdentifier
+				.getPortIdentifiers();
+		/*while (portEnum.hasMoreElements()) {
+			CommPortIdentifier portIdentifier = portEnum.nextElement();
+			System.out.println(portIdentifier.getName() + " - "
+					+ getPortTypeName(portIdentifier.getPortType()));
+		}*/
+	}
+
+	static String getPortTypeName(int portType) {
+		switch (portType) {
+		case CommPortIdentifier.PORT_I2C:
+			return "I2C";
+		case CommPortIdentifier.PORT_PARALLEL:
+			return "Parallel";
+		case CommPortIdentifier.PORT_RAW:
+			return "Raw";
+		case CommPortIdentifier.PORT_RS485:
+			return "RS485";
+		case CommPortIdentifier.PORT_SERIAL:
+			return "Serial";
+		default:
+			return "unknown type";
+		}
+	}
+
+	public static HashSet<CommPortIdentifier> getAvailableSerialPorts() {
+		HashSet<CommPortIdentifier> h = new HashSet<CommPortIdentifier>();
+		Enumeration thePorts = CommPortIdentifier.getPortIdentifiers();
+		while (thePorts.hasMoreElements()) {
+			CommPortIdentifier com = (CommPortIdentifier) thePorts
+					.nextElement();
+			switch (com.getPortType()) {
+			case CommPortIdentifier.PORT_SERIAL:
+				try {
+					CommPort thePort = com.open("CommUtil", 50);
+					thePort.close();
+					h.add(com);
+					System.out.println(com.getName());
+				} catch (PortInUseException e) {
+					System.out.println("Port, " + com.getName()
+							+ ", is in use.");
+				} catch (Exception e) {
+					System.err.println("Failed to open port " + com.getName());
+					e.printStackTrace();
+				}
+			}
+		}
+		return h;
+	}
+
+}
--- trunk/program/Can_Communication/src/com/tju/CanCommunication/test/Main.java
+++ trunk/program/Can_Communication/src/com/tju/CanCommunication/test/Main.java
@@ -1,15 +1,20 @@
 package com.tju.CanCommunication.test;
 
+import java.util.Enumeration;
+
 import com.tju.CanCommunication.Communication.*;
+import gnu.io.CommPortIdentifier;
 
 public class Main {
 
 	/**
 	 * @param args
+	 * @throws InterruptedException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		communicationTest();
+		testRXTX();
 	}
 	
 	public static void communicationTest(){
@@ -18,6 +23,21 @@ public class Main {
 		Command sendCmd = new Command(cmdStr);
 		mycommand.setCmd(sendCmd);
 		mycommand.sendCommand(sendCmd);
+	}
+	
+	public static void testRXTX() throws InterruptedException{
+		Enumeration portList = CommPortIdentifier.getPortIdentifiers(); //得到当前连接上的端口  
+        
+        CommUtil comm3 = new CommUtil(portList,"COM3"); 
+        int i = 0;  
+        while(i<1)  
+        {  
+            
+            comm3.send("C\r");  
+            Thread.sleep(300000);  
+            i++;  
+        }  
+        comm3.ClosePort(); 
 	}
 	
 	public static void operationTest(){
